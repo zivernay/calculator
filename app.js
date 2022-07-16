2dd23d24b35fd6f1bd0304ce8aa029404618cd53
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-16 14:26:36 +0300

    Put operators a in the object calculator

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-function add(x, y){
-    return x + y
-};
-
-function substract(x, y){
-    return x - y
-};
-
-function divide(x, y){
-    return (x / y == Infinity)? "0 error" : x / y
-};
-
-function multiply(x, y){
-    return x * y
+const calculator = {
+    add(x, y){
+        return x + y
+    },
+    substract(x, y){
+        return x - y
+    },
+    divide(x, y){
+        return (x / y == Infinity)? "0 error" : x / y
+    },
+    multiply(x, y){
+        return x * y
+    },
 };
