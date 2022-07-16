ee12fdf929ad4fe8ba9ee4bae0d0f2f33eca3325
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-16 14:17:28 +0300

    Add 4 basic binary arithmatic operators

---
+++ app.js
@@ -0,0 +1,15 @@
+function add(x, y){
+    return x + y
+};
+
+function substract(x, y){
+    return x - y
+};
+
+function divide(x, y){
+    return (x / y == Infinity)? "0 error" : x / y
+};
+
+function multiply(x, y){
+    return x * y
+};
