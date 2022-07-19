4714cb939edb038318889f866f1e85a7c44c2752
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-19 20:55:49 +0300

    Add calculate to compute 2 inputs

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const calculator = {
     nums : document.querySelectorAll("#keypad .num"),
     operators : document.querySelectorAll("#keypad .oparator"),
     screenDisplay : document.querySelector("#display"),
-    userIputs: {currentInput: "",arg1 : 0, arg2 : 0},
-
-    add(x, y){
+    calcKey : document.querySelector("#equal"),
+    userInputs: {currentInput: "",
+                expression: "",
+                arg1 : 0,
+                 arg2 : 0,
+                },
+    plus(x, y){
         return x + y
     },
-    substract(x, y){
+    subtract(x, y){
         return x - y
     },
     divide(x, y){
@@ -17,28 +21,36 @@ const calculator = {
         return x * y
     },
     operate(oparator, x, y){
-        return oparator(x, y)
+        const ans = calculator[oparator](x, y);
+        this.userInputs.answer = ans;
+        this.display(ans)
     },
     display(entry){
-        this.userIputs.currentInput += entry;
-        this.screenDisplay.textContent = this.userIputs.currentInput;
+        this.screenDisplay.textContent = entry;
     },
     press(event){
-        calculator.display(this.textContent);
+        const entry = this.textContent;
+        calculator.userInputs.currentInput += entry;
+        calculator.userInputs.expression += entry
+        calculator.display(calculator.userInputs.expression);
     },
     getOperator(){
-        const figure = calculator.userIputs.currentInput.match(/\d*\.?\d*/)
-        calculator.userIputs.arg1 = Number(figure);
-        calculator.userIputs.currentInput = "";
-        calculator.userIputs.operator = this.getAttribute("id");
-        console.table(this);
-        console.table(calculator.userIputs);
-    }
+        const figure = calculator.userInputs.currentInput.match(/\d*\.?\d*/)
+        calculator.userInputs.arg1 = Number(figure);
+        calculator.userInputs.currentInput = "";
+        calculator.userInputs.operator = this.getAttribute("id");
+    },
+    calculate(){
+        const figure = calculator.userInputs.currentInput.match(/\d*\.?\d*/)
+        calculator.userInputs.arg2 = Number(figure);
+        calculator.userInputs.currentInput = "";
+        calculator.userInputs.previousExpressin = calculator.userInputs.expression;
+        calculator.userInputs.expression = "";
+        calculator.operate(calculator.userInputs.operator, calculator.userInputs.arg1, calculator.userInputs.arg2);
+    },
 };
 //events
 calculator.nums.forEach((num)=>num.addEventListener("click", calculator.press))
-
 //This in the called method refers to the operator button
 calculator.operators.forEach((operator)=>{operator.addEventListener("click", calculator.press);operator.addEventListener("click", calculator.getOperator)});
-
-//seperate screen values and inputs
+calculator.calcKey.addEventListener("click", calculator.calculate)
