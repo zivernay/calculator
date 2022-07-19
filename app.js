e3d14540a136a2b55ebeb64f0ce597841067c741
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-19 17:45:03 +0300

    Add function to get and display operator

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const calculator = {
     nums : document.querySelectorAll("#keypad .num"),
-    operators : document.querySelectorAll(".keypad .operator"),
+    operators : document.querySelectorAll("#keypad .oparator"),
     screenDisplay : document.querySelector("#display"),
-    screenOutput: {line1: "", line2: ""},
+    userIputs: {currentInput: "",arg1 : 0, arg2 : 0},
 
     add(x, y){
         return x + y
@@ -19,13 +19,26 @@ const calculator = {
     operate(oparator, x, y){
         return oparator(x, y)
     },
-    display(inputs){
-        this.screenOutput.line1 += inputs;
-        this.screenDisplay.textContent = this.screenOutput.line1;
+    display(entry){
+        this.userIputs.currentInput += entry;
+        this.screenDisplay.textContent = this.userIputs.currentInput;
     },
+    press(event){
+        calculator.display(this.textContent);
+    },
+    getOperator(){
+        const figure = calculator.userIputs.currentInput.match(/\d*\.?\d*/)
+        calculator.userIputs.arg1 = Number(figure);
+        calculator.userIputs.currentInput = "";
+        calculator.userIputs.operator = this.getAttribute("id");
+        console.table(this);
+        console.table(calculator.userIputs);
+    }
 };
 //events
-calculator.nums.forEach((num)=>num.addEventListener("click", press))
-function press(e){
-    calculator.display(this.textContent);
-};
+calculator.nums.forEach((num)=>num.addEventListener("click", calculator.press))
+
+//This in the called method refers to the operator button
+calculator.operators.forEach((operator)=>{operator.addEventListener("click", calculator.press);operator.addEventListener("click", calculator.getOperator)});
+
+//seperate screen values and inputs
